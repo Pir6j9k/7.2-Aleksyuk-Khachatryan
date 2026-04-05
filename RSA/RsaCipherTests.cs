fc0d6e4e31c554723fb085b3476cde9fc759e914
Pir6j9k <hasulik2060@gmail.com>
2026-04-05 23:35:36 +0300

    Шифрование и дешифрование английского символа

---
+++ RSA/RsaCipherTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Numerics;
+using _7._2_Aleksyuk_Khachatryan;
+
+namespace RSA
+{
+    [TestClass]
+    public class RSACipherTests
+    {
+        [TestMethod]
+        public void TestRSA_EncryptDecrypt_EnglishChar()
+        {
+            var keys = RSACipher.GenerateKeys();
+            string original = "Abcd";
+
+            string encrypted = RSACipher.Encrypt(original, keys.PublicKey, keys.N);
+            string decrypted = RSACipher.Decrypt(encrypted, keys.PrivateKey, keys.N);
+
+            Assert.AreEqual(original, decrypted, "Ошибка дешифрования английского символа.");
+            Assert.AreNotEqual(original, encrypted, "Зашифрованный текст совпадает с оригиналом.");
+        }
+    }
+}
